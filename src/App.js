77128a6bcd62b537f4d85e851b86a1356d679e3f
RohanDhangar <rohandhangardhangarrohan@gmail.com>
2023-07-07 11:50:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.gyanibooks.com/library/get_dummy_notes/"
+      );
+      setData(response.data);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="table-container">
+      <h1 className="heading">Fetching Data from the API</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+            <th>Column 4</th>
+          </tr>
+        </thead>
+        {data ? (
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.user}</td>
+                <td>{item.title}</td>
+                <td>{item.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </table>
     </div>
   );
 }
